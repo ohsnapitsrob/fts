d18ec2dfc86b4d55fc8db5159edf05fb77826b1c
ohsnapitsrob <robholdersnaps@gmail.com>
2026-05-14 07:08:38 +0100

    Move National Trust icon URL into config

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 window.APP_CONFIG = {
   MAPTILER_KEY: "lfDgDwVrhGWudpjiHOqx",
   MAP_STYLE: "streets",
+  NT_ICON_URL: "https://images.pixieset.com/063553411/79737b7a99cf1e6442ac14468460ebc1-xxlarge.png",
   // Google Sheets tabs published as CSV:
   // Put the direct CSV "publish to web" URLs for each tab here.
   TITLE_METADATA_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=536921099&single=true&output=csv",
@@ -10,6 +11,6 @@ window.APP_CONFIG = {
     tv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=1386447371&single=true&output=csv",
     music_videos: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=226124969&single=true&output=csv",
     misc: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=82045520&single=true&output=csv",
-    games: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=1442332259&single=true&output=csv" 
+    games: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=1442332259&single=true&output=csv"
   }
 };
